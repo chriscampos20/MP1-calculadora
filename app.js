4c59550a1c8f1870f4be5b3685e768c4f710571e
chriscampos20 <chriscamosm19@gmail.com>
2024-05-30 11:28:09 -0600

    ultima subida

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const display = document.querySelector("#display");
-    const buttons = document.querySelectorAll("button");
-
-    buttons.forEach((btn) => {
-        btn.addEventListener("click", () => {
-            if (btn.id === "=") {
-                try {
-                    display.value = eval(display.value);
-                } catch {
-                    display.value = "Error";
-                }
-            } else if (btn.id === "ac") {
-                display.value = "";
-            } else if (btn.id === "de") {
-                display.value = display.value.slice(0, -1);
-            } else if (btn.id === "X") {
-                display.value += "*";
-            } else {
-                display.value += btn.id;
-            }
-        });
-    });
-});
+/**
+ * PROFE , HASTA AQUI PUDE LLEGAR POR EL MOMENTO
+ * AUN NO DOMINO JS PERO ME PONDRE AL DIA.
+ */
